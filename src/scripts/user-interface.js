b1ee7daa14d4691e93fc9654f1a38532c04335e0
CopyypoC <william.wang650@gmail.com>
2025-04-07 02:59:24 -0700

    Add new task DOM render

--- src/scripts/user-interface.js
+++ src/scripts/user-interface.js
@@ -1,14 +1,117 @@
-// New Project Button
-// New Task button
-export const newTaskBtn = document.querySelector('.new-task-btn');
-export const closeModalBtn = document.querySelector('.close-modal-btn');
+import { Project } from "./project";
+import { Task } from "./task";
+import { projectList } from "./project-list";
+
+let currentProject = '';
+
+const closeModalBtns = document.querySelectorAll('.close-modal-btn');
+closeModalBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.closest('dialog').close();
+    })
+})
+
 const newTaskModal = document.querySelector('.new-task-modal');
-newTaskBtn.addEventListener('click', () => {
-    newTaskModal.showModal();
+document.querySelector('.new-task-btn')
+    .addEventListener('click', () => {
+        newTaskModal.showModal();
 });
-closeModalBtn.addEventListener('click', () => {
-    newTaskModal.close();
+
+const newProjectModal = document.querySelector('.new-project-modal');
+document.querySelector('.new-project-btn')
+    .addEventListener('click', () => {
+        newProjectModal.showModal();
 });
+
+const editTaskModal = document.querySelector('.edit-task-modal');
+document.querySelector('.task-title')
+    .addEventListener('click', () => {
+        editTaskModal.showModal();
+});
+
+document.querySelector('.new-task-modal')
+    .addEventListener('submit', (e) => {
+        e.preventDefault();     
+        const newTitle = e.target.querySelector('#new-title');
+        const newDescription = e.target.querySelector('#new-description');
+        const newDueDate = e.target.querySelector('#new-due-date');
+        const newTime = e.target.querySelector('#new-time');
+        const newPriority = e.target.querySelector(
+            'input[name="new-priority"]:checked');
+        
+        const newTask = new Task(
+            newTitle.value, newDescription.value, newDueDate.value, 
+            newTime.value, Number(newPriority.value)
+        );
+        // currentProject.addTask(newTask);
+        displayNewTask(newTask);
+});
+
+const taskListContainer = document.querySelector('.task-list-container');
+function displayNewTask(newTask) {
+    const taskContainer = Object.assign(document.createElement('div'), {
+        className: 'task-container',
+        'data-uuid': newTask.uuid,
+    });
+
+    const taskCheckbox = Object.assign(document.createElement('input'), {
+        type: 'checkbox',
+        name: 'complete',
+        'aria-label': 'task-checkbox',
+        className: 'task-checkbox',
+    });
+
+    const taskTitle = Object.assign(document.createElement('p'), {
+        className: 'task-title',
+        textContent: newTask.title,
+    });
+
+    const taskDueDate = Object.assign(document.createElement('p'), {
+        className: 'task-due-date',
+        textContent: new Date(newTask.dueDate),
+    });
+
+    const taskPriority = Object.assign(document.createElement('p'), {
+        className: 'task-priority',
+        textContent: newTask.priority,
+    })
+
+    taskListContainer.appendChild(taskContainer);
+    taskContainer.append(taskCheckbox, taskTitle, taskDueDate, taskPriority);
+    taskListContainer.appendChild(document.createElement('hr'));
+}
+
+document.querySelector('.new-project-modal')
+    .addEventListener('submit', (e) => {
+        e.preventDefault();
+        const newProject = createNewProject();
+        projectList.addProject(newProject);
+        displayNewProject(newProject);
+})
+
+function createNewProject() {
+    const newProjectName = document.getElementById('#new-project-name');
+    return new Project(newProjectName.value);
+}
+
+function displayNewProject(newProject) {
+
+}
+
+document.querySelector('.project-list')
+    .addEventListener('click', (e) => {
+        if (e.target.className === 'project-title') {
+            // Switch currentproject
+            // 
+        }
+    })
+
+
+// 1. JS data
+// 2. Event listener to get input
+// 3. Update JS data
+// 4. Render to DOM
+
 
 // Project List switch project
 // Click on task
